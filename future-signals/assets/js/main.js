8266a0e408f4fb980b90bb4c24e26e12249e4b0c
Uriah Gray <u@u-p.co>
2019-08-30 08:50:59 +1000

    push updates

--- future-signals/assets/js/main.js
+++ future-signals/assets/js/main.js
@@ -1,50 +1,35 @@
-$(document).ready(function() {
-   
-	// This will add a class .large to any research item that is clicked
-    $('.research-item').click(function() {
-        $(this).toggleClass('large');
-    });
+$(document).ready(function(){
+	console.log("Hello World");
 
-    // This will add the class .active to any nav item that is clicked. 
-    // It will also remove any .active class to any other nav item
-    $(".nav-item").click(function() {
-        $('.nav-item').removeClass('active');
-        $(this).addClass("active");
-    });
+	$('.research-item').click(function(){
+		$(this).toggleClass('large');
+	});
 
-    // This variable will be called later, it hides all research items
-    var hideAll = function() {
-        $('.research-item').addClass('hide');
-    }
+	$('.nav-item').click(function(){
+		$('.nav-item').removeClass('active');
+		$(this).addClass('active');
+	});
 
-    // This .btn-all will reveal all 
-    // .research-items by removing .hide from all the divs
-    $('.btn-all').click(function() {
-        $('.research-item').removeClass('hide');
-    });
+	$('.btn-all').click(function(){
+		$('.research-item').removeClass('hide');
+	});
 
-    // This is an example of a button being clicked. 
-    // First it runs the above variable and hides everything
-    // Then it finds any div .research-item with .car class and 
-    // removes the .hide class
-    
-    $('.btn-car').click(function() {
-        hideAll();
-        $('.car').removeClass('hide');
-    });
+	var hideAll = function() {
+		$('.research-item').addClass('hide');
+	};
 
-    $('.btn-kinect').click(function() {
-        hideAll();
-        $('.kinect').removeClass('hide');
-    });
+	$('.btn-car').click(function(){
+		hideAll();
+		$('.car').removeClass('hide');
+	});
 
-    $('.btn-tracking').click(function() {
-        hideAll();
-        $('.tracking').removeClass('hide');
-    });
+	$('.btn-tracking').click(function(){
+		hideAll();
+		$('.tracking').removeClass('hide');
+	});
 
-    // Repeat same pattern for other buttons here
-
-
-
+	$('.btn-kinect').click(function(){
+		hideAll();
+		$('.kinect').removeClass('hide');
+	});
 });
