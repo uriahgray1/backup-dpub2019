9c24c18886a267aa8c6f46f3222a37e621744c65
Uriah Gray <u@u-p.co>
2019-09-01 22:48:10 +1000

    push signals

--- future-signals/assets/js/main.js
+++ future-signals/assets/js/main.js
@@ -1,35 +1,25 @@
 $(document).ready(function(){
-	console.log("Hello World");
+	console.log("hello world");
 
-	$('.research-item').click(function(){
-		$(this).toggleClass('large');
-	});
-
-	$('.nav-item').click(function(){
-		$('.nav-item').removeClass('active');
-		$(this).addClass('active');
-	});
-
-	$('.btn-all').click(function(){
-		$('.research-item').removeClass('hide');
-	});
-
-	var hideAll = function() {
+	$('.btn-profile').click(function(){
 		$('.research-item').addClass('hide');
-	};
-
-	$('.btn-car').click(function(){
-		hideAll();
-		$('.car').removeClass('hide');
+		
 	});
 
-	$('.btn-tracking').click(function(){
-		hideAll();
-		$('.tracking').removeClass('hide');
-	});
 
-	$('.btn-kinect').click(function(){
-		hideAll();
-		$('.kinect').removeClass('hide');
-	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
