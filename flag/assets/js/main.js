19dd86d3cb734e216d4fb8d87e89d8c5da24303b
Uriah Gray <u@u-p.co>
2019-09-13 01:46:33 +1000

    update flag and map

--- flag/assets/js/main.js
+++ flag/assets/js/main.js
@@ -19,9 +19,8 @@ $(document).ready(function() {
     ];
 
     var urls = [
-        "http://uriahgray1.github.io/geo-graphics/ip.html",
-        "https://uriahgray1.github.io/geo-graphics/checkers.html",
-        "https://rochelleoh.github.io/geo-graphics/circles.html",
+        "https://uriahgray1.github.io/geo-graphics/gafa.html",
+        "https://uriahgray1.github.io/geo-graphics/checkers-type.html",
         "https://ameliaclark.github.io/geo-graphics/checkers-type.html",
         "http://naomi-etheridge.github.io/geo-graphics/checkers-type.html",
         "https://uriahgray1.github.io/gg-example/geo-graphics/world.html",
@@ -34,15 +33,16 @@ $(document).ready(function() {
         "https://melike-yucel.github.io/geo-graphics/ip.html",
         "https://melike-yucel.github.io/geo-graphics/checkers.html",
         "https://saml2897.github.io/geo-graphics/checkers.html",
+        "https://uriahgray1.github.io/geo-graphics/hemisphere.html",
+        "https://uriahgray1.github.io/geo-graphics/map.html",
+        "https://uriahgray1.github.io/geo-graphics/blocks.html",
         "https://nicolechin.github.io/starter-kit-v1.09/checkers.html",
         "https://james-highfield.github.io/geo-graphics/circles.html",
         "https://taylahsinead.github.io/geo-graphics/checkers-type.html",
-        "https://rochelleoh.github.io/geo-graphics/circles.html",
         "https://gabbyparish.github.io/geo-graphics/checkers-type.html",
         "https://bingyu-zhou.github.io/Geo-graphics/circles.html",
         "https://linachan98.github.io/geo-graphics/circles.html ",
         "https://uriahgray1.github.io/gg-example/geo-graphics/blue.html"
-
     ]
 
     $(".flag").addClass(classes[~~(Math.random() * classes.length)]);
@@ -69,6 +69,10 @@ $(document).ready(function() {
 
         $("iframe").each(function() {
             $(this).attr('src', urls[~~(Math.random() * urls.length)]);
+            var framesrc = $(this).attr("src");
+             $(".urls").append(framesrc);
+             var objDiv = document.querySelector(".urls");
+            objDiv.scrollTop = objDiv.scrollHeight;
         });
 
     }
