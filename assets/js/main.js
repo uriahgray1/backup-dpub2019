25c442e2787f3046750fed6c8ff1a4664e7ea37f
Uriah Gray <u@u-p.co>
2019-10-25 00:58:05 +1100

    index style

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    var colours = [
+    'red', 
+    'green',
+    'blue',    
+    'yellow',
+    'purple',];
+
+	$('.fc').each(function() {
+    $(this).css('color', colours[Math.floor(Math.random() * colours.length)]);
+	});
+
+	$( ".fc" ).mouseover(function() {
+  		$(this).css('color', colours[Math.floor(Math.random() * colours.length)]);
+  		$('.tc').css('color', colours[Math.floor(Math.random() * colours.length)]);
+	});
+});
